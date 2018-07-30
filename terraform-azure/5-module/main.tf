62e87e49614fbc62789815250e467eed97bfbdd0
Amanda Jennings <ajennings@hashicorp.com>
2018-07-30 10:38:42 -0400

    variable update

--- terraform-azure/5-module/main.tf
+++ terraform-azure/5-module/main.tf
@@ -21,7 +21,7 @@ module "windowsserver" {
   source         = "Azure/compute/azurerm"
   version        = "1.1.5"
   location       = "${var.location}"
-  vm_hostname    = "demo-ancil-vm"
+  vm_hostname    = "demo-amanda-vm"
   admin_password = "${var.admin_password}"
   vm_os_simple   = "WindowsServer"
   public_ip_dns  = ["${var.windows_dns_prefix}"]
